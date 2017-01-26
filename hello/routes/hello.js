46cdc12c889d9cfe7c23d144e6e14730769fee0b
Jenny Hamer <jhamer@ucsd.edu>
2017-01-25 23:32:20 -0800

    Updated the Hello, WOrld node.js app

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,16 @@
  */
 
 exports.view = function(req, res){
+  // Query request parameters
+  var nameToShow = req.params.userName;
+  // if no name specified, set default: World
+  if (nameToShow == undefined) {
+    nameToShow = 'World';
+  }
+
+  console.log("name is " + nameToShow);
+    
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
